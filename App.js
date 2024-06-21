751ccbcd4ce6c0f0187485ee33cacd6bce14dc0e
SankuriLaya <sankuri.laya@gmail.com>
2024-06-21 12:42:18 +0530

    todolist

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './TodoList'; // Adjust the path if TodoList.js is in a different directory
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
